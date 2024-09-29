013409a6212ac4dd010b5fd429c22b9b50153e66
taloreff <taloreff@gmail.com>
2024-09-29 18:49:30 +0300

    changed url

--- backend/api/client.js
+++ backend/api/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // export default axios.create({ baseURL: `http://192.168.3.5:5000` }); // yuval pc
 
-export default axios.create({ baseURL: `http://10.100.102.8:5000` }); // tal pc
+export default axios.create({ baseURL: `http://10.100.102.12:5000` }); // tal pc
 // export default axios.create({ baseURL: `http://10.100.102.8:5000` }); // tal fake pc
 
 // export default axios.create({ baseURL: `http://10.0.0.15:5000` });  //sassy pc
